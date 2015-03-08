010d3f9135945c5159f959f1e36c735ec6ae52dc
unknown <v-mengxu@6VZH122.mslpa.corp.microsoft.com>
2015-03-08 20:39:30 +0800

    New Version

--- ppCrawler.py
+++ ppCrawler.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 start_url_list = ['http://invest.ppdai.com/loan/list_safe', 'http://invest.ppdai.com/loan/list_payfor', 'http://invest.ppdai.com/loan/list_riskmiddle',
 					'http://invest.ppdai.com/loan/list_riskhigh', 'http://invest.ppdai.com/loan/list_bd​']
+page_range = [(1, 3), (1, 46), (1, 10), (1, 46), (1, 2)]
 
 request_params = {	'Connection': 'keep-alive',
 					'Cache-Control': 'max-age=0',
@@ -14,12 +16,23 @@ request_params = {	'Connection': 'keep-alive',
 					'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4'
 				}
 
+def unique(items):
+    keep = []
+    for item in items:
+        if item not in keep:
+            keep.append(item)
+    return keep
+
 def do_request(url):
-	r = requests.get(url, headers = request_params, timeout = 10)
-	if r.status_code != 200:
-		print ("Http request error: " + str(r.status_code) + " ---- " + url)
-		return ""
-	return r.content
+	while True:
+		try:
+			r = requests.get(url, headers = request_params, timeout = 10)
+			if r.status_code != 200:
+				print ("Http request error: " + str(r.status_code) + " ---- " + url)
+				return ""
+			return r.content
+		except Exception:
+			continue
 
 def parse_load_list_page(url):
 	html_content = do_request(url)
@@ -28,29 +41,40 @@ def parse_load_list_page(url):
 	soup = BeautifulSoup(html_content)
 	ret = []
 
-	load_list = soup.find_all(name = 'a', class_ = 'title ell')
-	for item in load_list:
-		ret.append(item['href'])
+	p = soup.find_all(name = 'p', class_ = 'userInfo clearfix')
+	for item in p:
+		ret.append(item.find(name = 'a')['href'])
 
 	return ret
 
 def parse_user_page(url):
 	html_content = do_request(url)
 	soup = BeautifulSoup(html_content)
-	ret = []
+	ret_info = []
+	user_info = []
 
+	# Get user information
 	li = soup.find(name = 'li', class_ = 'honor_li')
 	span = li.find_all(name = 'span', class_ = 'cf7971a')
 	for item in span:
-		ret.append(item.text)
+		user_info.append(item.text)
 
 	li = soup.find(name = 'li', class_ = 'user_li')
 	span = li.find_all(name = 'span')
-	ret.append(span[0].text)
+	user_info.append(span[0].text)
 
-	return ret
+	# Get load urls
+	div = soup.find_all(name = 'div', class_ = 'rightlist fl')
+	for items in div:
+		if items.text.find('100%') == -1:
+			continue
+		host = 'http://www.ppdai.com'
+		s_url = host + items.find(name = 'a')['href']
+		ret_info.append(parse_load_single_page(s_url, user_info))
 
-def parse_load_single_page(url):
+	return ret_info
+
+def parse_load_single_page(url, info):
 	html_content = do_request(url)
 	soup = BeautifulSoup(html_content)
 	ret_info = []
@@ -66,9 +90,7 @@ def parse_load_single_page(url):
 	ret_info.append(div[0].text)
 
 	# 借入信用等级，借出信息等级，性别
-	a = soup.find(name = 'a', class_ = 'username')
-	user_info = parse_user_page(a['href'])
-	ret_info = ret_info + user_info
+	ret_info = ret_info + info
 
 	# 历史成功次数，历史流标次数
 	div = soup.find(name = 'div', class_ = 'lendDetailContent_infoDetail_userInfo clearfix')
@@ -83,27 +105,48 @@ def parse_load_single_page(url):
 
 	return ret_info
 
-def output_to_file(file, info_list):
-	for item in info_list:
+def output_to_file(file, info):
+	for item in info:
 		file.write(item.replace('\n', '').replace('\r', '').replace(' ', '') + ", ")
 	file.write('\n')
 
 def main():
 	# Open data file in append mode
-	data_file = open('data.txt', encoding = 'utf-8', mode = 'a')
+	if len(sys.argv) < 2:
+		print ("Please specify output file name")
+		return
+	file_name = sys.argv[1]
+	data_file = open(file_name, encoding = 'utf-8', mode = 'a')
+	user_url_list = []
 
-	for start_url in start_url_list:
-		while True:
-			page = 1
+	s_len = len(start_url_list)
+	for i in range(s_len):
+		start_url = start_url_list[i]
+		for page in range(page_range[i][0], page_range[i][1]):
 			page_url = start_url + "_s0_p" + str(page)
-			res = parse_load_list_page(page_url)
-			if len(res) == 0:
-				continue
-			for item in res:
-				info = parse_load_single_page(item)
-				output_to_file(data_file, info)
-			print ("Finish page: " + page_url) 
-			page = page + 1
+			ret_user = parse_load_list_page(page_url)
+			user_url_list = user_url_list + ret_user
+			# print ('Parsing page: ' + page_url + '......')
+			# res = parse_load_list_page(page_url)
+			# if len(res) == 0:
+			# 	break
+			# for item in res:
+			# 	info = parse_load_single_page(item)
+			# 	if info[9].find('100%') != -1:
+			# 		output_to_file(data_file, info)
+			# 	handled_page_url.append(item)
+			# print ('Finish page: ' + page_url) 
+			# page = page + 1
+
+	# Remove duplicated user urls
+	user_url_list = unique(user_url_list)
+
+	for item in user_url_list:
+		print ('Parsing user: ' + item)
+		info_list = parse_user_page(item)
+		for info in info_list:
+			output_to_file(data_file, info)
+		print (str(len(info_list)) + " items added")
 
 if __name__ == '__main__':
 	main()
